c40046e928c1bebc2e09fe30caa8fb6b2fb94bd8
alpaslan <yeniturkalpaslan4@gmail.com>
2024-05-05 22:24:58 +0300

    Ekrandaki sayının tek mi çift mi olduğunu anlayıp duruma göre renk değiştiren kod yapısı

--- sayiAnlamaProjesi/ContentView.swift
+++ sayiAnlamaProjesi/ContentView.swift
@@ -1,21 +1,41 @@
-//
-//  ContentView.swift
-//  sayiAnlamaProjesi
-//
-//  Created by ALPASLAN YENİTÜRK on 5.05.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Durum değişkenleri
+    @State private var number: Int = 1
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Sayıya bağlı olarak metni görüntüle
+            Text("\(number)")
+                .font(.title)
+            // Düğme için yatay yığın
+            HStack {
+                // Sayıyı artırma düğmesi
+                Button(action: {
+                    self.number += 1
+                }) {
+                    Text("Artır")
+                }
+                // Sayıyı sıfırlama düğmesi
+                Button(action: {
+                    self.number = 1
+                }) {
+                    Image(systemName: "arrow.counterclockwise") // Sıfırla simgesi
+                    Text("Sıfırla") // Simge yanındaki metin
+                }
+            }
+            // Sayının çift mi tek mi olduğunu kontrol et
+            if number % 2 == 0 {
+                Text("Çift Sayı")
+                    .foregroundColor(.blue)
+            } else {
+                Text("Tek Sayı")
+                    .foregroundColor(.red)
+            }
         }
-        .padding()
+        .font(.title)
     }
 }
 
