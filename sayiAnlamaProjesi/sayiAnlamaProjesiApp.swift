6a3dab6770b323abedbf9d054cca220e32b17f18
alpaslan <yeniturkalpaslan4@gmail.com>
2024-05-05 22:22:38 +0300

    Initial Commit

---
+++ sayiAnlamaProjesi/sayiAnlamaProjesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sayiAnlamaProjesiApp.swift
+//  sayiAnlamaProjesi
+//
+//  Created by ALPASLAN YENİTÜRK on 5.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct sayiAnlamaProjesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
